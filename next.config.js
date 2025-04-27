eb3be0b16a70a125a3298db542735b4f9396624f
lpengal <luka@meettippy.com>
2025-04-27 10:36:30 +0200

    Fix static asset serving2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
   output: 'export',
   images: {
     unoptimized: true,
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
   },
 }
 
