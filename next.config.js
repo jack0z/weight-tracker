f79f535fe9c32a77cf25e559c5aa49781dd2d02c
lpengal <luka@meettippy.com>
2025-04-27 21:16:08 +0200

    netlify config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+        child_process: false,
+        'timers/promises': false
+      }
+    }
+    return config
+  }
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
